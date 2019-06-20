0431a2a97a2adc77e71cbbafe9c949ab58913c64
Drew Palazzari <drewpalazzari@hotmail.com>
2019-06-20 10:34:57 -0600

    Refactor out async/await

--- tests/requestor.weather.test.js
+++ tests/requestor.weather.test.js
@@ -1,9 +1,11 @@
-test('Requestor retrieves data for Denver from weather API', async () => {
+test('Requestor retrieves data for Denver from weather API', () => {
   const consulWeather = require("../helpers/consulWeather");
   const requestor = require('../services/requestor');
-  const config = await consulWeather()
-  const city = 'Denver'
-  const weatherUrl = `http://api.openweathermap.org/data/2.5/weather?appid=${config}&units=metric&q=${city}`
-  const data = await requestor(weatherUrl)
-  expect(typeof data).toBe('object')
+  consulWeather().then(config => {
+    const city = 'Denver'
+    const weatherUrl = `http://api.openweathermap.org/data/2.5/weather?appid=${config}&units=metric&q=${city}`
+    requestor(weatherUrl).then(data => {
+      expect(typeof data).toBe('object')
+    }).catch(err => console.log(err))
+  }).catch(err => console.log(err))
 })
