3e4635f30a68b68947d3d9d2ed4e978c081b5fa8
Drew Palazzari <drewpalazzari@hotmail.com>
2019-06-22 12:55:19 -0600

    Add hacker and reddit routes/tests

--- routes/articleRoute.js
+++ routes/articleRoute.js
@@ -2,9 +2,27 @@ module.exports = (app) => {
   const consulArticles = require("../helpers/consulArticles");
   const requestor = require('../services/requestor');
 
-  app.get("/api/v1/nerdbay/articles", (req, res) => {
+  app.get("/api/v1/nerdbay/ign", (req, res) => {
     consulArticles().then(config => {
       const articlesUrl = `https://newsapi.org/v1/articles?apiKey=${config}&source=ign&sortBy=top`
+      requestor(articlesUrl).then(data => {
+        res.send(data)
+      }).catch(err => res.status(404).send(err.response))
+    }).catch(err => res.status(404).send(err))
+  })
+
+  app.get("/api/v1/nerdbay/hackernews", (req, res)=> {
+    consulArticles().then(config => {
+      const articlesUrl = `https://newsapi.org/v1/articles?apiKey=${config}&source=hacker-news&sortBy=top`
+      requestor(articlesUrl).then(data => {
+        res.send(data)
+      }).catch(err => res.status(404).send(err.response))
+    }).catch(err => res.status(404).send(err))
+  })
+
+  app.get("/api/v1/nerdbay/reddit", (req, res) => {
+    consulArticles().then(config => {
+      const articlesUrl = `https://newsapi.org/v1/articles?apiKey=${config}&source=reddit-r-all&sortBy=top`
       requestor(articlesUrl).then(data => {
         res.send(data)
       }).catch(err => res.status(404).send(err.response))
--- tests/routes/routes.articles.test.js
+++ tests/routes/routes.articles.test.js
@@ -2,8 +2,20 @@ const request = require('supertest');
 const app = require('../../app')
 
 describe('Test the Articles path', () => {
-  test('It should respond with 200 for GET', () => {
-    return request(app).get('/api/v1/nerdbay/articles').then(response => {
+  test('It should respond with 200 for GET IGN', () => {
+    return request(app).get('/api/v1/nerdbay/ign').then(response => {
+      expect(response.statusCode).toBe(200);
+    })
+  });
+
+  test('It should respond with 200 for GET Hacker News', () => {
+    return request(app).get('/api/v1/nerdbay/hackernews').then(response => {
+      expect(response.statusCode).toBe(200);
+    })
+  });
+
+  test('It should respond with 200 for GET Reddit - all', () => {
+    return request(app).get('/api/v1/nerdbay/reddit').then(response => {
       expect(response.statusCode).toBe(200);
     })
   });
