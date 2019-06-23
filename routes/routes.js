defd7268baee6bf30c4ce8d98b6b7a6ec1f8541a
Drew Palazzari <drewpalazzari@hotmail.com>
2019-06-23 16:28:20 -0600

    Add twitch route

--- routes/routes.js
+++ routes/routes.js
@@ -1,11 +1,13 @@
-module.exports = (app) => {
-  const ekgRouter = require('../routes/ekgRoute')
-  const weatherRouter = require('../routes/weatherRoute')
-  const articleRouter = require('../routes/articleRoute')
-  const movieRouter = require('../routes/movieRoute')
+const ekgRouter = require('../routes/ekgRoute')
+const weatherRouter = require('../routes/weatherRoute')
+const articleRouter = require('../routes/articleRoute')
+const movieRouter = require('../routes/movieRoute')
+const twitchRouter = require('../routes/twitchRoute')
 
+module.exports = (app) => {
   ekgRouter(app)
   weatherRouter(app)
   articleRouter(app)
   movieRouter(app)
+  twitchRouter(app)
 }
