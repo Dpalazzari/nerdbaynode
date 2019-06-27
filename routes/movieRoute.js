b6f52ae0a7da219e808302479f80a5b582c93358
Drew Palazzari <drewpalazzari@hotmail.com>
2019-06-26 21:03:10 -0600

    Update movie route with cache obj

--- routes/movieRoute.js
+++ routes/movieRoute.js
@@ -1,13 +1,44 @@
-module.exports = (app) => {
-  const consulMovie = require("../helpers/consulMovie");
-  const requestor = require('../services/requestor');
+const { cacheSetter, cacheResetter, cacheGetter } = require('../cache/initCache');
+const requestor = require('../services/requestor');
+const NodeCache = require("node-cache");
+let myCache = new NodeCache();
+const consulMovie = require('../helpers/consulMovie');
+
+module.exports = async (app) => {
+  const key = 'movies';
+  const interval = 60 * 60 * 1000;
+  let movieUrl, config;
+
+  try {
+    config = await consulMovie();
+    movieUrl = `https://api.themoviedb.org/3/movie/now_playing?api_key=${config}`;
+    console.log('Pulling down Movies Cache...');
+    myCache = await cacheSetter(movieUrl, key);
+  } catch(err){
+    console.error(err);
+  }
+
+  setInterval( async() => {
+    try {
+      console.log('Pulling down new movies cache...');
+      const value = await cacheResetter(myCache, key);
+      config = await consulMovie();
+      movieUrl = `https://api.themoviedb.org/3/movie/now_playing?api_key=${config}`;
+      myCache = (value) ? await cacheSetter(movieUrl, key) : new NodeCache();
+    } catch (err) {
+      console.error(err);
+    }
+  }, interval);
 
   app.get("/api/v1/nerdbay/movies", (req, res) => {
-    consulMovie().then(config => {
-      const movieUrl = `https://api.themoviedb.org/3/movie/now_playing?api_key=${config}`
+    cacheGetter(myCache, 'movies')
+    .then(movieData => {
+      res.send(movieData);
+    }).catch(err => {
+      console.log('cache is null...')
       requestor(movieUrl).then(data => {
         res.send(data)
       }).catch(err => res.status(404).send(err.response))
-    }).catch(err => res.status(404).send(err))
+    })
   })
 }
